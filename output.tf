40d9c5d7f4e52b71ba8f496e1955a954afe909dd
unknown <prathmeshdahikar123@gmail.com>
2025-12-26 17:53:21 +0530

    khatta metha

---
+++ output.tf
@@ -0,0 +1,18 @@
+# output.tf
+
+output "cluster_id" {
+  value = aws_eks_cluster.devopsshack1.id
+}
+
+output "node_group_id" {
+  value = aws_eks_node_group.devopsshack1_node_group.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.devopsshack1_vpc.id
+}
+
+output "subnet_ids" {
+  value = aws_subnet.devopsshack1_subnet[*].id
+}
+
